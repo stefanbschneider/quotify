dcfb7a94e56dd645bcd42b97d2e61aa1cdbb16a2
Stefan Schneider <stefan.schneider@upb.de>
2020-07-01 21:33:30 +0200

    set static root

--- quotify/settings.py
+++ quotify/settings.py
@@ -117,5 +117,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
+# path to where static files are copied for deployment (eg, for heroku)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
