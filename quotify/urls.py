9568a227edcc4c4dc0ccb547616ea0753c4d18e0
Stefan Schneider <stefan.schneider@upb.de>
2020-07-01 21:45:29 +0200

    show quotes app on base url /

--- quotify/urls.py
+++ quotify/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('quotes/', include('quotes.urls')),
+    # was: 'quotes/'
+    path('', include('quotes.urls')),
     path('admin/', admin.site.urls),
 ]
