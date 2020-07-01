24e4fee8e04fc607aa4827f026b461ed4939a6b8
Stefan Schneider <stefan.schneider@upb.de>
2020-07-01 21:54:25 +0200

    configured django for heroku: procfile, gunicorn, django_heroku

--- quotify/settings.py
+++ quotify/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -119,4 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
 # path to where static files are copied for deployment (eg, for heroku)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
